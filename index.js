5457ca35af6e9c8607aea4b24c60c22c7f634679
J-ustine <78701219+J-ustine@users.noreply.github.com>
2021-03-05 19:53:01 +0100

    Add button to play the game

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 let randomNumberRight = Math.ceil(Math.random() * 6);
 let randomNumberLeft = Math.ceil(Math.random() * 6);
 
-document
-  .querySelector(".img1")
-  .setAttribute("src", `images/dice${randomNumberLeft}.png`);
-document
-  .querySelector(".img2")
-  .setAttribute("src", `images/dice${randomNumberRight}.png`);
+function playGame() {
+  document
+    .querySelector(".img1")
+    .setAttribute("src", `images/dice${randomNumberRight}.png`);
+  document
+    .querySelector(".img2")
+    .setAttribute("src", `images/dice${randomNumberLeft}.png`);
 
-if (randomNumberRight > randomNumberLeft) {
-  document.querySelector("h1").innerHTML = "Player 1 wins !";
-} else {
-  document.querySelector("h1").innerHTML = "Player 2 wins !";
+  if (randomNumberRight > randomNumberLeft) {
+    document.querySelector("h1").innerHTML = "Player 1 wins !";
+  } else {
+    if (randomNumberRight < randomNumberLeft) {
+      document.querySelector("h1").innerHTML = "Player 2 wins !";
+    } else {
+      document.querySelector("h1").innerHTML = "Eguality! Play again!";
+    }
+  }
 }
+
+document.querySelector(".play-btn").addEventListener("click", playGame);
