be7bac099ca886acfdf730bb289a56b4cce29988
J-ustine <78701219+J-ustine@users.noreply.github.com>
2021-03-06 15:34:42 +0100

    Create Add player button and make the game works for 6players

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-let randomNumberRight = Math.ceil(Math.random() * 6);
-let randomNumberLeft = Math.ceil(Math.random() * 6);
+function createRandomNumbers() {
+  console.log(numberPlayer);
+  /*  randomNumbers += for (let a = 0; a<numberPlayer; a++){
+      Math.ceil(Math.random() * 6)
+  }*/
+  for (let i = randomNumbers.length; i < numberPlayer; i++) {
+    randomNumbers.push(Math.ceil(Math.random() * 6));
+  }
+  console.log(randomNumbers);
+  console.log(Math.max.apply(Math, randomNumbers));
+  return randomNumbers.indexOf(Math.max.apply(Math, randomNumbers));
+}
 
 function playGame() {
-  document
-    .querySelector(".img1")
-    .setAttribute("src", `images/dice${randomNumberRight}.png`);
-  document
-    .querySelector(".img2")
-    .setAttribute("src", `images/dice${randomNumberLeft}.png`);
-
-  if (randomNumberRight > randomNumberLeft) {
-    document.querySelector("h1").innerHTML = "Player 1 wins !";
-  } else {
-    if (randomNumberRight < randomNumberLeft) {
-      document.querySelector("h1").innerHTML = "Player 2 wins !";
-    } else {
-      document.querySelector("h1").innerHTML = "Eguality! Play again!";
+  createRandomNumbers();
+  let duplicate = 0;
+  for (let n = 0; n < randomNumbers.length; n++) {
+    if (randomNumbers[n] === Math.max.apply(Math, randomNumbers)) {
+      duplicate++;
     }
+  }
+  for (let i = 0; i < randomNumbers.length; i++) {
+    document
+      .querySelector(`.img${i + 1}`)
+      .setAttribute("src", `images/dice${randomNumbers[i]}.png`);
+  }
+
+  if (duplicate > 1) {
+    document.querySelector("h1").innerHTML = `Drawn ! Play again!`;
+  } else {
+    document.querySelector("h1").innerHTML = `Player ${
+      createRandomNumbers() + 1
+    } wins !`;
   }
 }
 
+function addPlayer() {
+  if (numberPlayer < 6) {
+    document.querySelector(
+      ".row"
+    ).innerHTML += `<div class="col"><div class="dice"><p>Player ${
+      numberPlayer + 1
+    }</p><img class='img${
+      numberPlayer + 1
+    }' src='images/dice6.png' width='100px'/></div></div>`;
+    numberPlayer++;
+  } else {
+    alert("Sorry, only 6 people can play to the Dicee Game!");
+  }
+}
+let numberPlayer = 2;
+let randomNumbers = [];
+
 document.querySelector(".play-btn").addEventListener("click", playGame);
+document.querySelector(".add-player-btn").addEventListener("click", addPlayer);
